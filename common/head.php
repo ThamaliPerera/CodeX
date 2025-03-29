af0907fcd896eaa6004721224d0df149a058795a
Rashmika200217 <rashmikadhananjaya@gmail.com>
2025-03-29 11:22:24 +0530

    Delete common directory

--- common/head.php
+++
@@ -1,8 +0,0 @@
-<!-- Common Meta Tags for SEO -->
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-<!-- CSS -->
-<link href="css/bootstrap.min.css" rel="stylesheet">
-<link href="css/style.css" rel="stylesheet">
